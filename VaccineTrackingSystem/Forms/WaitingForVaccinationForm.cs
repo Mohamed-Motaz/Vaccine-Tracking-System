03cc79dd9d13295ef99fc59be84c9067c3245a8e
salma tarek <62105437+salmaatarekk@users.noreply.github.com>
2021-05-25 00:40:19 +0200

    Waiting List 3azama

--- VaccineTrackingSystem/Forms/WaitingForVaccinationForm.cs
+++ VaccineTrackingSystem/Forms/WaitingForVaccinationForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VaccineTrackingSystem.DataModels;
 
 namespace VaccineTrackingSystem.Forms
 {
@@ -15,6 +16,96 @@ namespace VaccineTrackingSystem.Forms
         public WaitingForVaccinationForm()
         {
             InitializeComponent();
+        }
+        List<User> tmp = new List<User>() { new User()
+            {
+                UserName = "Mohamed Motaz",
+                NationalID = "hscdjkcd",
+                Password = "dcnlkc",
+                Gender = "Male",
+                Age = 15,
+                Governorate = "Giza",
+                Vaccinated = 1
+        },
+        new User()
+        {
+            UserName = "Ali Agina",
+                NationalID = "hscdjkcd",
+                Password = "dcnlkc",
+                Gender = "Male",
+                Age = 15,
+                Governorate = "Giza",
+                Vaccinated = 1
+            },
+          new User()
+        {
+            UserName = "Kareem Ezzat",
+                NationalID = "hscdjkcd",
+                Password = "dcnlkc",
+                Gender = "Male",
+                Age = 15,
+                Governorate = "Giza",
+                Vaccinated = 1
+            },
+            new User()
+        {
+            UserName = "AbdelAzim",
+                NationalID = "hscdjkcd",
+                Password = "dcnlkc",
+                Gender = "Male",
+                Age = 15,
+                Governorate = "Giza",
+                Vaccinated = 1
+            }, new User()
+        {
+            UserName = "Rawan Ahmed",
+                NationalID = "hscdjkcd",
+                Password = "dcnlkc",
+                Gender = "Male",
+                Age = 15,
+                Governorate = "Giza",
+                Vaccinated = 1
+            }, new User()
+        {
+            UserName = "Salma Tarek",
+                NationalID = "hscdjkcd",
+                Password = "dcnlkc",
+                Gender = "Male",
+                Age = 15,
+                Governorate = "Giza",
+                Vaccinated = 1
+            },
+            };
+        private void WaitingForVaccinationForm_Load(object sender, EventArgs e)
+        {
+
+            foreach(User newUser in tmp)
+            {
+                ListItem newList = new ListItem();
+                newList.name = newUser.UserName;
+                newList.id = newUser.NationalID ;
+                newList.pass = newUser.Password;
+                newList.Gender = newUser.Gender;
+                newList.age = newUser.Age;
+                newList.contry = newUser.Governorate;
+               
+
+                if (flowLayoutPanel1.Controls.Count < 0)
+                    flowLayoutPanel1.Controls.Clear();
+                else
+                    flowLayoutPanel1.Controls.Add(newList);
+            }
+        }
+
+
+        private void listItem1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
--- VaccineTrackingSystem/Program.cs
+++ VaccineTrackingSystem/Program.cs
@@ -17,7 +17,7 @@ namespace VaccineTrackingSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AdminControlForm());
+            Application.Run(new WaitingForVaccinationForm());
         }
     }
 }
